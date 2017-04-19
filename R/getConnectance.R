16e7dfd20b9f19467a8f2ea0cd384ae8103f6c71
Leo Lahti <leo.lahti@iki.fi>
2017-04-19 14:57:34 +0200

    Fixed encoding issue in getConnectance.R

--- R/getConnectance.R
+++ R/getConnectance.R
@@ -1,20 +1,22 @@
-#' Compute connectance of an interaction matrix.
-#'
-#' The connectance is defined as E/Nˆ2-N, where E is the number
+#' @title Connectance
+#' @description Compute connectance of an interaction matrix.
+#' @details The connectance is defined as $E/N^2-N$, where $E$ is the number
 #' of realized arcs (the number of non-zero entries in the interaction matrix)
-#' and N*(N-1) the number of possible arcs.
+#' and $N*(N-1)$ the number of possible arcs.
 #' The diagonal (self-arcs) is excluded.
 #'
-#' @param A interaction matrix
+#' @param A an interaction matrix
 #' @return the connectance
 #' @examples
-#' A=cbind(c(-1,0,1),c(0,-1,0),c(-1,0,-1))
-#' getConnectance(A)
+#'   A <- cbind(c(-1,0,1),c(0,-1,0),c(-1,0,-1))
+#'   x <- getConnectance(A)
 #' @export
+getConnectance <- function(A){
 
-getConnectance<-function(A){
-	 N=nrow(A)
+	 N <- nrow(A)
+	 
 	 # exclude diagonal from observed and possible interactions
-	 c=(length(A[A!=0])-N)/(ncol(A)*ncol(A)-N)
+	 c <- (length(A[A!=0])-N)/(ncol(A)*ncol(A)-N)
+	 
 	 return(c)
  }
