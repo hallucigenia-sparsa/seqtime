f0d4ed76386c71d9e0404a54fe8193b0d9cf7ad2
Lisa Röttjers <u0118219@kuleuven.be>
2017-12-21 13:58:07 +0100

    Added functionality for glv and function for generating datasets

--- R/glv.R
+++ R/glv.R
@@ -11,16 +11,41 @@
 #' @param tstart initial time point
 #' @param tend final time point
 #' @param tstep time step
+#' @param perturb perturbation object describing growth changes
 #' @return a matrix with species abundances as rows and time points as columns, column names give time points
 #' @seealso \code{\link{ricker}} for the Ricker model
 #' @examples
 #' tsplot(glv(N=4,generateA(4)),header="gLV")
 #' @export
 
-glv<-function(N=4,A,b=runif(N),y=runif(N),tstart=0,tend=100,tstep=0.1){
+glv<-function(N=4,A,b=runif(N),y=runif(N),tstart=0,tend=100,tstep=0.1, perturb=NULL){
+  # checks perturb object and includes in parms
   # parms as matrix
   parms=cbind(b,A)
   parms=cbind(rep(N,N),parms)
+  if (!is.null(perturb)){
+    if (length(perturb$growthchanges) != N){
+      stop("Please provide as many growth changes as species!")
+    }
+    else {
+      growthchanges = perturb$growthchanges
+      parms = cbind(parms,growthchanges)
+      if (length(perturb$times) != 0){
+        if (length(perturb$times) > N){
+          stop("Number of perturbations needs to be less than the number of species!")
+        }
+        else {
+          count = length(perturb$times)
+          times = vector(length = N, mode="numeric")
+          times[1:(length(perturb$times))] = perturb$times
+          durations = vector(length = N, mode="numeric")
+          durations[1:(length(perturb$durations))] = perturb$durations
+          parms = cbind(parms, times, durations)
+          parms = cbind(parms, rep(count, N))
+        }
+      }
+    }
+  }
   times<-seq(tstart, tend, by=tstep)
   # run the simulation
   commtime<-lsoda(y, times, glvsolve, parms)
@@ -30,7 +55,6 @@ glv<-function(N=4,A,b=runif(N),y=runif(N),tstart=0,tend=100,tstep=0.1){
   colnames(commtime)=time
   return(commtime)
 }
-
 
 # ==============================================================
 # Equations (generalized Lotka-Volterra)
@@ -45,6 +69,26 @@ glvsolve<-function(t, y, parms){
   N=parms[1,1]  # species number
   b=parms[,2]   # vector of growth rates
   A=parms[,3:(N+2)] # interaction matrix
+  if (length(parms[1,]) == (N+3)){
+    c=parms[,(N+3)]
+    b = b + c
+  }
+  else if (length(parms[1,]) > (N+3)){
+    c=parms[,(N+3)]
+    count = parms[1,(N+6)]
+    times = parms[,(N+4)]
+    durations = parms[,(N+5)]
+    intervals = list()
+    for (i in 1:count){
+      tstart = times[i]
+      tend = times[i] + durations[i]
+      if ((t>tstart) & (t<tend)){
+        b = b + c
+        print(b)
+      }
+    }
+  }
   dydt <- y*(b+A %*% y)
   list(dydt)
 }
+
---
+++ R/removeLowAbundance.R
@@ -0,0 +1,23 @@
+#' @title Remove lowest abundance species
+#'
+#' @description Removes a number of lowest total abundace species from a dataset generated with generateDataSet.
+#' Note that the function uses the normalized dataset to decide which species to remove, but returns the absolute dataset.
+#'
+#' @param dataset Abundance dataset
+#' @param removeN Number of species to remove
+#' @return Dataset without N lowest abundance species
+#' @export
+
+# Takes generated dataset and removes (lowest abundance) species
+# Simulates effects of removing rare OTUs
+removeLowAbundance = function(dataset, removeN){
+  dataset2 = seqtime::normalize(dataset)
+  vec = vector(mode = "numeric")
+  for (i in 1:removeN){
+    vec = rowSums(dataset2)
+    min = which.min(vec)
+    dataset = dataset[-min,]
+  }
+  return(dataset)
+}
+
